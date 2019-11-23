18afd30b23365627635716743e628f6c77841718
ChoiysApple <chcs1370@gmail.com>
2019-11-24 00:15:02 +0900

    restore commits

---
+++ neARism/app/src/main/java/com/marchengraffiti/nearism/nearism/course/ChooseCourseActivity.java
@@ -0,0 +1,74 @@
+package com.marchengraffiti.nearism.nearism.course;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import com.marchengraffiti.nearism.nearism.R;
+import com.marchengraffiti.nearism.nearism.firebase.CourseRead;
+import com.marchengraffiti.nearism.nearism.firebase.MyCallback;
+
+import java.util.ArrayList;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChooseCourseActivity extends AppCompatActivity {
+
+    String subnum, detailimg, subname, overview, cid;
+
+    ImageButton back;
+
+    private ArrayList<CourseDetailItem> data = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_course);
+
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        final ListView listView = findViewById(R.id.detailList);
+
+        CourseRead courseRead = new CourseRead();
+        courseRead.ReadCourseList(new MyCallback() {
+            @Override
+            public void onCallback(String value) {
+                String[] value_split = value.split("#");
+                subnum = value_split[0]; // 코스 장소 순서
+                detailimg = value_split[1]; // 각 코스 장소마다 이미지
+                subname = value_split[2]; // 코스 장소 이름
+                overview = value_split[3]; // 코스 장소 상세 설명
+                cid = value_split[4]; // 메인 코스랑 장소 매칭시키려고
+
+                CourseDetailItem item = new CourseDetailItem(subnum, detailimg, subname, overview);
+                data.add(item);
+
+                // course_item.xml 은 코스 메인화면에 보이는 리스트뷰
+                // 코스 메인화면 레이아웃에 arrayList 세팅
+                CourseDetailAdapter adapter = new CourseDetailAdapter(getApplicationContext(), R.layout.course_detail_item, data);
+                listView.setAdapter(adapter);
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Intent intent = new Intent(getApplicationContext(), AddCourseActivity.class);
+                        intent.putExtra("detailimg", data.get(position).getDetailimg());
+                        intent.putExtra("subname", data.get(position).getSubname());
+                        intent.putExtra("overview", data.get(position).getOverview());
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
+
+    }
+}
