16f6eb99e11d6f5bd9982585b7059bef33c42d33
Daegun Choi <43776784+ChoiysApple@users.noreply.github.com>
2019-11-27 18:59:10 +0900

    reupload app

---
+++ neARism/app/src/main/java/com/marchengraffiti/nearism/nearism/ARActivity.java
@@ -0,0 +1,13 @@
+package com.marchengraffiti.nearism.nearism;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ARActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
